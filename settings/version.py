2780ac325e1e366429dfa31663048cdb097bb42d
neutrobox <neutrobox@gmail.com>
2024-07-08 19:13:44 -0400

    Update version.py

--- settings/version.py
+++ settings/version.py
@@ -2,4 +2,4 @@ MAIN_VERSION = '1.2.0'
 SUB_VERSION = ''
 VERSION = MAIN_VERSION + SUB_VERSION
 
-CHANGE_LOG = '* Added support for Sunqua Peak CM.\n* Adjusted Nightmare and Shattered Observatory fractal numbers to 98 and 99.'
+CHANGE_LOG = '* Compatibilidad con los nuevos nombres de las carpetas de logs de bosses en ArcDPS. '
