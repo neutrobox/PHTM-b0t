1dc0708b10ac1a6ae6bd81eeb0aa043eb8624c74
neutrobox <neutrobox@gmail.com>
2024-12-08 11:43:04 -0400

    Fix

--- settings/version.py
+++ settings/version.py
@@ -1,5 +1,5 @@
-MAIN_VERSION = '1.2.0'
+MAIN_VERSION = '1.2.1'
 SUB_VERSION = ''
 VERSION = MAIN_VERSION + SUB_VERSION
 
-CHANGE_LOG = '* Compatibilidad con los nuevos nombres de las carpetas de logs de bosses en ArcDPS. '
+CHANGE_LOG = '* Compatibilidad para Wing 8. '
